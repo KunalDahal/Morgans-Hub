ff7f63af2d9e7b5735801143a6e11553544dd138
Subaru Natsuki <dahal1kunal@gmail.com>
2025-07-19 13:16:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,8 +9,13 @@ echo "Downloading Chrome..."
 wget -q https://storage.googleapis.com/chrome-for-testing-public/138.0.7204.157/linux64/chrome-linux64.zip
 unzip -q chrome-linux64.zip
 mv chrome-linux64 /tmp/chrome/chrome
-
-# Clean up
 rm -f chrome-linux64.zip
+echo "✅ Chrome installed at /tmp/chrome/chrome"
 
-echo "✅ Chrome up in /tmp/chrome"
+echo "Downloading ChromeDriver..."
+wget -q https://storage.googleapis.com/chrome-for-testing-public/138.0.7204.157/linux64/chromedriver-linux64.zip
+unzip -q chromedriver-linux64.zip
+mv chromedriver-linux64/chromedriver /tmp/chrome/chromedriver
+chmod +x /tmp/chrome/chromedriver
+rm -rf chromedriver-linux64.zip chromedriver-linux64
+echo "✅ ChromeDriver installed at /tmp/chrome/chromedriver"
